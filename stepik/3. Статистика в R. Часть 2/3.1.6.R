690cbf46958e4ad3cac7624d0613a091cd9ab99b
Yerkebulan <erke.zhumagali@gmail.com>
2018-06-19 15:14:14 +0600

    13th commit

--- stepik/3. Статистика в R. Часть 2/3.1.6.R
+++ stepik/3. Статистика в R. Часть 2/3.1.6.R
@@ -1,6 +1,9 @@
 regr.calc <- function(x){
-  if () { 
-    return() 
+test_1 <- cor.test(x[, 1], x[, 2])
+  if (test_1$p.value < 0.05) { 
+    fit <- lm(x[, 1] ~ x[, 2])
+    x$fit <- fit$fitted.values
+    return(x)
   } else { 
     return("There is no sense in prediction")}
     
